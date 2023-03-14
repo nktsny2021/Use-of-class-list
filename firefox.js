e9d0c5ab4953c9503b556317bcfb5d5641314c40
中田伸也 <nakatashinya@nakatashinyanoMacBook-Pro.local>
2023-03-14 13:34:29 +0900

    figt commit

--- firefox.js
+++ firefox.js
@@ -1,38 +1,55 @@
-'use strict'    
-const targetImg = document.getElementById(`main`);
-console.log(targetImg);
+'use strict'
 const btn = document.getElementById('update');
 console.log(btn);
-const classes = ["Aframe", "Bframe", "Cframe", "Dframe", "Eframe", "Gframe"]; // 適用するクラス名を配列で定義
-
-let currentClassIndex = 0; // 現在適用中のクラス名のインデックスを初期化
 let isReversing = false; // 適用を逆順にするためのフラグを初期化
+const targetElement = document.getElementById('main');
+console.log(targetElement);
 
-btn.addEventListener("click", function() {
-  targetImg.classList.remove(classes[currentClassIndex]); // 現在適用中のクラス名を削除
-
-  if (!isReversing) {
-    currentClassIndex++; // インデックスをインクリメント
-    if (currentClassIndex === classes.length) {
-      isReversing = true; // インデックスが最後まで達したらフラグを立てる
-      currentClassIndex--; // インデックスをデクリメントして逆順にする
-    }
-  } else {
-    currentClassIndex--; // インデックスをデクリメント
-    if (currentClassIndex === -1) {
-      isReversing = false; // インデックスが最初に戻ったらフラグを解除
-      currentClassIndex++; // インデックスをインクリメントして元に戻す
-    }
-  }
-  targetImg.classList.add(classes[currentClassIndex]); // 次のクラス名を適用
+let currentIndex = 0; // 現在適用中のクラス名のインデックスを初期化
+const classes = ["Aframe", "Bframe", "Cframe", "Dframe", "Eframe", "Gframe"]; // 適用するクラス名を配列で定義
+btn.addEventListener('click', function() {
+  targetElement.classList.remove(classes[currentIndex]);
+  currentIndex = (currentIndex + 1) % classes.length;
+  targetElement.classList.add(classes[currentIndex]);
 });
 
-let rebtn = document.getElementById(`reset`);
-console.log(rebtn);
-rebtn.addEventListener( `click`, function reset(clsClean) {
-    var clsClean = document.getElementById('main');
-    clsClean.removeAttribute(`class`);
-    console.log(clsClean);
+
+// btn.addEventListener('click', function() {
+//   targetImg.classList.remove(classes[currentClassIndex]); // 現在適用中のクラス名を削除
+//   currentClassIndex++;
+//   if (currentClassIndex >= classes.length) {
+//     currentClassIndex = 0;
+//   }
+//   targetImg.classList.add(classes[currentClassIndex]); // 次のクラス名を適用
+// });
+
+
+  // if (!isReversing) {
+  //   currentClassIndex++; // インデックスをインクリメント
+  //   if (currentClassIndex === classes.length) {
+  //     isReversing = true; // インデックスが最後まで達したらフラグを立てる
+  //     currentClassIndex--; // インデックスをデクリメントして逆順にする
+  //   }
+  // } else {
+  //   currentClassIndex--; // インデックスをデクリメント
+  //   if (currentClassIndex === -1) {
+  //     isReversing = false; // インデックスが最初に戻ったらフラグを解除
+  //     currentClassIndex++; // インデックスをインクリメントして元に戻す
+  //   }
+  // }
+//   targetImg.classList.add(classes[currentClassIndex]); // 次のクラス名を適用
+// });
+
+const resetbtn = document.getElementById('reset');
+console.log(resetbtn);
+resetbtn.addEventListener('click', function() {
+targetElement.classList.add("Aframe");
+classes.forEach(function (className) {
+targetElement.classList.remove(className) 
+});
+currentIndex = 0;
+isReversing = false;
+console.log(targetElement);
 });
   
   // startとendが逆転っしたら終わり
